bd0ab2abf60bea929ff84a462df589e931dd178c
Vladimir Khromov <vlad2aria@mail.ru>
2022-08-23 21:28:48 +0300

    1.6.9

--- 1.6.9.py
+++ 1.6.9.py
@@ -4,28 +4,48 @@ TYPE_OS = 1 # 1 - Windows; 2 - Linux
 
 class DialogWindows:
 	name_class = "DialogWindows"
-	def __init__(self, name):
-		self.name = name  
 
 class DialogLinux:
 	name_class = "DialogLinux"
-	def __init__(self, name):
-		self.name = name  
+
 
 class Dialog:
 	def __new__(cls, *args, **kwarg):
+		obj = None
 		if TYPE_OS == 1:
-			ret = DialogWindows.__new__(cls)
+			obj = super().__new__(DialogWindows)
 		else:
-			ret = DialogLinux.__new__(cls)
+			obj = super().__new__(DialogLinux)
 
-		ret.name = args[0]
-		return ret
+		obj.name = args[0]
+		return obj
 
 	def __init__(self, name):
 		self.name = name  
+
+
+
+
+
+## TEST ## 
 
 
 dlg = Dialog("Строка")		
 
-print(dlg, dlg.__dict__)
+
+
+
+TYPE_OS = 1
+dlg_1 = Dialog("123")
+TYPE_OS = 2
+dlg_2 = Dialog("1234")
+
+assert isinstance(dlg_1, DialogWindows) and isinstance(dlg_2, DialogLinux), "создаваемые объекты не соответствуют нужным классам DialogWindows или DialogLinux"
+
+assert dlg_1.name == "123", "неверное значение локального атрибута name класса DialogWindows"
+assert dlg_2.name == "1234", "неверное значение локального атрибута name класса DialogLinux"
+
+d1 = Dialog("12")
+d2 = Dialog("123")
+
+assert d1.name == "12" and d2.name == "123", "неверные значения в локальных атрибутах name разных объектов класса DialogLinux"
--- 3.3.11.py
+++ 3.3.11.py
@@ -1,59 +1,15 @@
-class Recipe:
-	def __init__(self, *agrs):
-		self.recipes = list(agrs)
+class PolyLine:
+	def __init__(self, *args):
+		self.args = list(args)
 
 
+	def add_coord(self, x, y):
+		self.args.append((x, y))
+
+	def remove_coord(self, indx):
+		self.args.pop(indx)
+
+	def get_coords(self):
+		return self.args
 
 
-	def add_ingredient(self, ing):
-		if type(ing) == Ingredient:
-			self.recipes.append(ing)
-
-
-	def get_ingredients(self):
-		return tuple(self.recipes)
-
-
-	def remove_ingredient(self, ing):
-		if ing in self.recipes:
-			self.recipes.remove(ing)
-
-
-	def __len__(self):
-		return len(self.recipes)
-
-
-
-class Ingredient:
-	def __init__(self, name, volume, measure):
-		self.name = name  
-		self.volume = volume
-		self.measure = measure
-
-	def __str__(self):
-		return f"{self.name}: {self.volume}, {self.measure}"
-
-
-
-
-## TEST ##
-
-i1 = Ingredient("Соль", 1, "столовая ложка")
-i2 = Ingredient("Мука", 1, "кг")
-i3 = Ingredient("Мясо баранины", 10, "кг")
-i4 = Ingredient("Масло", 100, "гр")
-recipe = Recipe(i1, i2, i3)
-recipe.add_ingredient(i4)
-recipe.remove_ingredient(i3)
-
-assert len(recipe) == 3, "функция len вернула неверное значение"
-lst = recipe.get_ingredients()
-for x in lst:
-    assert isinstance(x, Ingredient), "в списке рецептов должны быть только объекты класса Ingredient"
-    assert hasattr(x, 'name') and hasattr(x, 'volume') and hasattr(x, 'measure'), "объект класса Ingredient должен иметь атрибуты: name, volume, measure"
-
-assert str(i4) == "Масло: 100, гр", "метод __str__ вернул неверное значение"
-
-i4 = Ingredient("Масло", 120, "гр")
-recipe.add_ingredient(i4)
-assert len(recipe) == 4, "функция len вернула неверное значение"
